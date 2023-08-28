3a98ebd12e2536f204aabe53b8a70ba7fa03587d
Gabriel Cunha <gabrielcunha2012@gmail.com>
2023-08-28 14:25:55 -0300

    botões do footer funcional

--- home.js
+++ home.js
@@ -3,3 +3,12 @@ const carouselElement = document.querySelector('#carouselExampleDark');
 carouselElement.addEventListener('slide.bs.carousel', event => {
     console.log("assererre")
 })
+
+function goToWhatsapp() {
+    window.open('https://wa.me/85988481999', '_blank');
+}
+
+function goToInstagram() {
+    window.open('https://www.instagram.com/j.gabriel.cj/', '_blank');
+}
+
