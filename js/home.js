6b06bcc9412cc68844e5a60d5b5e81398646814e
Gabriel Cunha <gabrielcunha2012@gmail.com>
2023-09-01 17:36:30 -0300

    início da implementação do modal do produto

--- js/home.js
+++ js/home.js
@@ -24,6 +24,10 @@ function closeMenu() {
     $('.background-menu').removeClass('open');
 }
 
+function closeProduto() {
+    $('.background-produto').removeClass('open');
+}
+
 function goToWhatsapp() {
     window.open('https://wa.me/5585988481999', '_blank');
 }
@@ -38,16 +42,31 @@ $(document).ready(function() {
         let produto = {
             nome: $(this).find("label").text(),
             valor: $(this).find("span").text(),
-            quantidade: 1
+            quantidade: 1,
+            tamanho: '',
+            imagePath: $(this).find(".card-image").css('backgroundImage'),
         }
 
-        let index = carrinho.itens.findIndex(({nome}) => nome == produto.nome);
-        if(index != -1) carrinho.itens[index].quantidade += 1;
-        else {
-            carrinho.itens.push(produto);
-            carrinho.quantidade++;
-            $('#carrinho-btn span').text((carrinho.quantidade).toString());
-        }
+        $('.background-produto').addClass('open');
+
+        let url = produto.imagePath;
+        $('.produto-container .produto-body').html(
+            '<div class="produto-image" style="background-image: ' + url.replaceAll('"', "'") + ';"></div>' +
+            '<div class="produto-info">' + 
+                '<h5>' + produto.nome + '</h5>' +
+                '<label> Tamanho: ' + produto.tamanho + '</label>' +
+                '<label> Valor: ' + produto.valor + '</label>' +
+            '</div>');
+
+        // let index = carrinho.itens.findIndex(({nome}) => nome == produto.nome);
+        // if(index != -1) carrinho.itens[index].quantidade += 1;
+        // else {
+        //     carrinho.itens.push(produto);
+        //     carrinho.quantidade++;
+        //     $('#carrinho-btn span').text((carrinho.quantidade).toString());
+        // }
+
+        // $('.carrinho-btn span').removeClass('d-none');
     });
 
 });
