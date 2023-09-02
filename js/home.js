21c5344c1f9f62fc767e6711726cdcd0472d5674
GabrielCunha <gabrielcunha2012@gmail.com>
2023-09-02 14:56:45 -0300

    tamanho no modal da camisa

--- js/home.js
+++ js/home.js
@@ -1,72 +1,80 @@
-
 var carrinho = {
-    itens: [],
-    valorTotal: 0,
-    quantidade: 0
+  itens: [],
+  valorTotal: 0,
+  quantidade: 0,
 };
 
 function toggleMenu(element) {
-    if(element.checked) {
-        $('.background-menu').addClass('open');
-    } else {
-        $('.background-menu').removeClass('open');
-    }
+  if (element.checked) {
+    $(".background-menu").addClass("open");
+  } else {
+    $(".background-menu").removeClass("open");
+  }
 }
 
 function openModalCarrinho() {
-    $('.background-carrinho').addClass('open');
-
-    
-
+  $(".background-carrinho").addClass("open");
 }
 
 function closeMenu() {
-    $('.background-menu').removeClass('open');
+  $(".background-menu").removeClass("open");
 }
 
 function closeProduto() {
-    $('.background-produto').removeClass('open');
+  $(".background-produto").removeClass("open");
 }
 
 function goToWhatsapp() {
-    window.open('https://wa.me/5585988481999', '_blank');
+  window.open("https://wa.me/5585988481999", "_blank");
 }
 
 function goToInstagram() {
-    window.open('https://www.instagram.com/j.gabriel.cj/', '_blank');
+  window.open("https://www.instagram.com/j.gabriel.cj/", "_blank");
 }
 
-$(document).ready(function() {
-    
-    $('.card-produto').on('click', function() {
-        let produto = {
-            nome: $(this).find("label").text(),
-            valor: $(this).find("span").text(),
-            quantidade: 1,
-            tamanho: '',
-            imagePath: $(this).find(".card-image").css('backgroundImage'),
-        }
+$(document).ready(function () {
+  $(".input-tamanho button").on("click", function () {
+    $(".input-tamanho button").removeClass("selected");
+    $(this).addClass("selected");
+  });
 
-        $('.background-produto').addClass('open');
+  $(".card-produto").on("click", function () {
+    let produto = {
+      nome: $(this).find("label").text(),
+      valor: $(this).find("span").text(),
+      quantidade: 1,
+      tamanho: "",
+      imagePath: $(this).find(".card-image").css("backgroundImage"),
+    };
 
-        let url = produto.imagePath;
-        $('.produto-container .produto-body').html(
-            '<div class="produto-image" style="background-image: ' + url.replaceAll('"', "'") + ';"></div>' +
-            '<div class="produto-info">' + 
-                '<h5>' + produto.nome + '</h5>' +
-                '<label> Tamanho: ' + produto.tamanho + '</label>' +
-                '<label> Valor: ' + produto.valor + '</label>' +
-            '</div>');
+    $(".background-produto").addClass("open");
 
-        // let index = carrinho.itens.findIndex(({nome}) => nome == produto.nome);
-        // if(index != -1) carrinho.itens[index].quantidade += 1;
-        // else {
-        //     carrinho.itens.push(produto);
-        //     carrinho.quantidade++;
-        //     $('#carrinho-btn span').text((carrinho.quantidade).toString());
-        // }
+    let url = produto.imagePath;
+    $(".produto-container .produto-body").html(
+      '<div class="produto-image" style="background-image: ' +
+        url.replaceAll('"', "'") +
+        ';"></div>' +
+        '<div class="produto-info">' +
+        "<h5>" +
+        produto.nome +
+        "</h5>" +
+        "<label> Tamanho: " +
+        produto.tamanho +
+        "</label>" +
+        "<label> Valor: " +
+        produto.valor +
+        "</label>" +
+        "</div>"
+    );
 
-        // $('.carrinho-btn span').removeClass('d-none');
-    });
+    // let index = carrinho.itens.findIndex(({nome}) => nome == produto.nome);
+    // if(index != -1) carrinho.itens[index].quantidade += 1;
+    // else {
+    //     carrinho.itens.push(produto);
+    //     carrinho.quantidade++;
+    //     $('#carrinho-btn span').text((carrinho.quantidade).toString());
+    // }
 
+    // $('.carrinho-btn span').removeClass('d-none');
+  });
 });
